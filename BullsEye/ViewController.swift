49255b19abbbd94d4b4dfc6153d6ed59ac32b229
Larissa <lbarra@thoughtworks.com>
2018-03-26 16:38:51 -0300

    [refactor] lbarra: removes unnecessary method

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -28,11 +28,6 @@ class ViewController: UIViewController {
         startNewGame()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
     @IBAction func onSliderMove(_ sender: UISlider) {
         currentValue = lroundf(sender.value)
     }
