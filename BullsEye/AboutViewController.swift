9fb48c885a2c706d6b3a04996870bb2aef1904ae
Larissa <lbarra@thoughtworks.com>
2018-03-26 16:34:37 -0300

    [logic] lbarra: adds about screen

---
+++ BullsEye/AboutViewController.swift
@@ -0,0 +1,16 @@
+//
+//  AboutViewController.swift
+//  BullsEye
+//
+//  Created by Larissa Barra Conde on 26/03/18.
+//  Copyright © 2018 CapDev ThoughtWorks. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+    
+    @IBAction func closeInfo(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+}
